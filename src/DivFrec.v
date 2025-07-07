762db9db1a4bc52ebd064da7c1be6a2ec83c6ab9
jpulidof <jpulidof@unal.edu.co>
2025-07-07 02:04:38 -0500

    update

--- src/DivFrec.v
+++ src/DivFrec.v
@@ -1,8 +1,15 @@
-module DivFrec (
+module DivFrec(
     input clk,
     output reg Q
 );
+    reg [17:0] count = 0;  // 18 bits alcanzan hasta 262143
+
     always @(posedge clk) begin
-        Q <= ~Q;
+        if (count >= 199_999) begin
+            count <= 0;
+            Q <= ~Q;
+        end else begin
+            count <= count + 1;
+        end
     end
 endmodule

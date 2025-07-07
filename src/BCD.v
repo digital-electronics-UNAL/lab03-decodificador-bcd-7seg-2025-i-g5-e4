798686b268474f4922b2cc90e436336f5b7712d2
jpulidof <jpulidof@unal.edu.co>
2025-07-06 23:24:25 -0500

    update con sumador y todas las cajas

--- src/BCD.v
+++ src/BCD.v
@@ -1,22 +1,29 @@
-module BCD (
-    input clk,
-    input [4:0] num,        
-    output reg [3:0] BCD
+module BCD(
+    input [8:0] bin,
+    output reg [3:0] BCD2,
+    output reg [3:0] BCD1,
+    output reg [3:0] BCD0
 );
+    integer i;
+    reg [20:0] shift_reg;
 
-    reg algo = 0;
-              
+    always @(*) begin
+        shift_reg = 21'd0;
+        shift_reg[8:0] = bin;
 
-    always @(posedge clk) begin
-        case (algo)
-            1'b0: begin
-                BCD <= num % 10;  
-                algo <= 1'b1;
-            end
-            1'b1: begin
-                BCD <= (num-num%10)/ 10;  
-                algo <= 1'b0;
-            end
-        endcase
+        for (i = 0; i < 9; i = i + 1) begin
+            if (shift_reg[11:8] >= 5)
+                shift_reg[11:8] = shift_reg[11:8] + 3;
+            if (shift_reg[15:12] >= 5)
+                shift_reg[15:12] = shift_reg[15:12] + 3;
+            if (shift_reg[19:16] >= 5)
+                shift_reg[19:16] = shift_reg[19:16] + 3;
+
+            shift_reg = shift_reg << 1;
+        end
+
+        BCD0 = shift_reg[11:8];
+        BCD1 = shift_reg[15:12];
+        BCD2 = shift_reg[19:16];
     end
 endmodule

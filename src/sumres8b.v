ab129ff0540fc67b2834bbb9f044b7809b4ef5e7
jpulidof <55963767+jpulidof@users.noreply.github.com>
2025-07-09 23:23:19 -0500

    suma

--- src/sumres8b.v
+++ src/sumres8b.v
@@ -2,14 +2,14 @@ module sumres8b (
     input  [7:0] A,
     input  [7:0] B,
     input        Sel,     // 0 = suma, 1 = resta
-    output signed [8:0] resultado // 9 bits, firmado
+    output signed [8:0] resultado
 );
     reg signed [8:0] tmp;
     always @(*) begin
         if (Sel)
-            tmp = {1'b0, A} - {1'b0, B}; // opera siempre entre 0 y 255
+            tmp = $signed(A) - $signed(B);
         else
-            tmp = {1'b0, A} + {1'b0, B};
+            tmp = $signed(A) + $signed(B);
     end
     assign resultado = tmp;
 endmodule

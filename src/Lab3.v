a95298748f8649c940ea58f44ca254965aca9eca
jpulidof <55963767+jpulidof@users.noreply.github.com>
2025-07-09 22:38:18 -0500

    sin negar b

--- src/Lab3.v
+++ src/Lab3.v
@@ -10,7 +10,7 @@ module Lab3 (
 );
 
     wire [7:0] A = {~A_fpga[7], ~A_fpga[6], ~A_fpga[5], ~A_fpga[4], ~A_fpga[3], ~A_fpga[2], ~A_fpga[1], ~A_fpga[0]};
-    wire [7:0] B = {~B_fpga[7], ~B_fpga[6], ~B_fpga[5], ~B_fpga[4], ~B_fpga[3], ~B_fpga[2], ~B_fpga[1], ~B_fpga[0]};
+    wire [7:0] B = {B_fpga[7], B_fpga[6], B_fpga[5], B_fpga[4], B_fpga[3], B_fpga[2], B_fpga[1], B_fpga[0]};
     wire Sel = ~Sel_fpga;
     wire [8:0] resultado;
 

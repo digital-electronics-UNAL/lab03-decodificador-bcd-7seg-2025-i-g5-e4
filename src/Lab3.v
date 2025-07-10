2c70bb9f76541926c332953668bcf5611a0b490a
jpulidof <55963767+jpulidof@users.noreply.github.com>
2025-07-09 23:46:57 -0500

    pruebas

--- src/Lab3.v
+++ src/Lab3.v
@@ -9,7 +9,12 @@ module Lab3 (
 
     // Cambia el orden o la negación según cómo estén tus switches físicos:
     // Ejemplo 1: Switch derecho = A_fpga[0]
-    wire [7:0] A = {~A_fpga[7], ~A_fpga[6], ~A_fpga[5], ~A_fpga[4], ~A_fpga[3], ~A_fpga[2], ~A_fpga[1], ~A_fpga[0]};
+    wire [7:0] A = {~A_fpga[6], ~A_fpga[5], ~A_fpga[4], ~A_fpga[3], ~A_fpga[2], ~A_fpga[1], ~A_fpga[0], ~A_fpga[7]};
+    //wire [7:0] A = {~A_fpga[5], ~A_fpga[4], ~A_fpga[3], ~A_fpga[2], ~A_fpga[1], ~A_fpga[0], ~A_fpga[7], ~A_fpga[6]};
+    //wire [7:0] A = {~A_fpga[0], ~A_fpga[1], ~A_fpga[2], ~A_fpga[3], ~A_fpga[4], ~A_fpga[5], ~A_fpga[6], ~A_fpga[7]};
+    //wire [7:0] A = {~A_fpga[1], ~A_fpga[2], ~A_fpga[3], ~A_fpga[4], ~A_fpga[5], ~A_fpga[6], ~A_fpga[7], ~A_fpga[0]};
+
+
     // Ejemplo 2: Si el derecho es A_fpga[7], usa {~A_fpga[0], ..., ~A_fpga[7]}
 
     wire [8:0] valor = {1'b0, A}; // siempre positivo

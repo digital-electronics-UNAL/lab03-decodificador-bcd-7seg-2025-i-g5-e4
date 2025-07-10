7d84d4cd6cfb3c3b86d07d03bea248c517291f9a
jpulidof <55963767+jpulidof@users.noreply.github.com>
2025-07-09 23:37:00 -0500

    cambio

--- src/Lab3.v
+++ src/Lab3.v
@@ -9,14 +9,16 @@ module Lab3 (
     output [3:0] an
 );
 
-    wire [7:0] A = {~A_fpga[0], ~A_fpga[1], ~A_fpga[2], ~A_fpga[3], ~A_fpga[4], ~A_fpga[5], ~A_fpga[6], ~A_fpga[7]};
-    wire [7:0] B = {~B_fpga[0], ~B_fpga[1], ~B_fpga[2], ~B_fpga[3], ~B_fpga[4], ~B_fpga[5], ~B_fpga[6], ~B_fpga[7]};
+    wire [7:0] A = {~A_fpga[7], ~A_fpga[6], ~A_fpga[5], ~A_fpga[4], ~A_fpga[3], ~A_fpga[2], ~A_fpga[1], ~A_fpga[0]};
+    wire [7:0] B = {B_fpga[7], B_fpga[6], B_fpga[5], B_fpga[4], B_fpga[3], B_fpga[2], B_fpga[1], B_fpga[0]};
+    wire [7:0] A_forzada = A >> 1;
+    wire [7:0] B_forzada = B >> 1;
     wire Sel = ~Sel_fpga;
     wire signed [8:0] resultado;
 
     Sumador9b alu (
-        .A(A),
-        .B(B),
+        .A(A_forzada),
+        .B(B_forzada),
         .Sel(Sel),
         .resultado(resultado)
     );
@@ -29,5 +31,6 @@ module Lab3 (
     );
 
 endmodule
+
 
 

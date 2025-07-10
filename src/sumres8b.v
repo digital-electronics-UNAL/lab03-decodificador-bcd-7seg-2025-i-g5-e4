5654e0076ccd3030cde055918cb8889402846f4e
jpulidof <55963767+jpulidof@users.noreply.github.com>
2025-07-09 22:46:14 -0500

    cambio resta

--- src/sumres8b.v
+++ src/sumres8b.v
@@ -2,14 +2,14 @@ module sumres8b (
     input  [7:0] A,
     input  [7:0] B,
     input        Sel,     // 0 = suma, 1 = resta
-    output [8:0] resultado // 9 bits, firmado
+    output signed [8:0] resultado // 9 bits, firmado
 );
-    reg [8:0] tmp;
+    reg signed [8:0] tmp;
     always @(*) begin
         if (Sel)
-            tmp = {1'b0, A} - {1'b0, B}; // resta en 9 bits (con signo)
+            tmp = $signed({1'b0, A}) - $signed({1'b0, B}); // resta firmada
         else
-            tmp = {1'b0, A} + {1'b0, B}; // suma en 9 bits
+            tmp = $signed({1'b0, A}) + $signed({1'b0, B}); // suma firmada
     end
     assign resultado = tmp;
 endmodule
